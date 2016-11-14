5a11fabc41538e9c520b1206b6bd749f1e14f585
ittus <vuminhthang.cm@gmail.com>
2016-11-14 13:57:07 +0800

    Add Test API Key

--- services/config.js
+++ services/config.js
@@ -1,5 +1,5 @@
-const API_KEY = '';
-const SECRET_KEY = '';
+const API_KEY = 'c01ab516764677eec0d5';
+const SECRET_KEY = 'ecbab3105b932d3bed0fb693e191b41b036527ad';
 
 module.exports = {
     API_KEY: API_KEY,
